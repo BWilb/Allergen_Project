e15676dd7fc716255f070d33c0b9d7fd61acef3a
BWilb <bryce.wilbur@my.simpson.edu>
2022-10-23 17:12:31 -0500

    updated webscraping class

--- src/webscrape_class.js
+++ src/webscrape_class.js
@@ -1,88 +1,49 @@
-/*
-class will later be used for scraping entire web page
-for items
-*/
-
-const puppeteer = require("puppeteer");
-//module for webscraping
 class Object{
-    constructor(url, property, xfactor) {
+    constructor(url, element, attribute, class_name){
         this.url = url
-        // variable for url
-        this.property = property
-        // variable for specific property
-        this.xfactor = xfactor
-        // variable for xfactor of html object
+        this.element = element
+        //variable for specific html element
+        this.attribute = attribute
+        //variable for elements attributes
+        this.class_name = class_name
+        //variable for elements class
+        this.axios = require("axios")
+        this.cheerio = require("cheerio")
     }
 
-    async retrieve_url(){
-        //retrieves url
-        const browser = await puppeteer.launch();
-        // creation of browser variable
-        const page = await browser.newPage();
-        // page variable
-        await page.goto(this.url)
-        // page retrieves specific url
+    find_element(params) {
+        this.axios(this.url)
+            .then(response => {
+                const html_script = response.data
+                const values = []
+                const honey = this.cheerio.load(html_script)
+                honey(this.class_name, html_script).each(function(){
+                    //function created to search through entire script 
+                    const text = honey(this).text()
+                    const att = honey(this).find(this.element).attr(this.attribute)
 
-        const [el] =  await page.$x(this.xfactor);
-        // page retrieves xfactor
-        const text = await el.getProperty(this.property);
-        // page retrieves property
-        const object = text.jsonValue();
-
-        console.log({object});
-        browser.close();
+                    values.push({
+                    //appending specific text to array of values
+                        text
+                    })
+                })
+                var i = 0
+                while (i < values.length){
+                    if(i < values.length){
+                        console.log(values[i].text)
+                    }
+                    else{
+                        break;
+                    }
+                    i += 1
+                }
+            }).catch(err => console.log(err))
+        
     }
 }
 
-function retreive(){
+objectOne = new Object('https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall',
+    'a', 'href', '.get-nutritioncalculator')
 
-    objectOne = new Object("https://simpsondining.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'textContent',
-        '/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[1]/h1');
-    // first object(Name of place)
-    objectOne.retrieve_url();
 
-    objectTwo = new Object("https://simpsondining.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'src',
-        '/html/body/div[1]/div/div[2]/div[1]/div/img');
-    // second object(pfeiffer image)
-    objectTwo.retrieve_url();
-
-    objectThree = new Object("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'placeholder',
-        /*
-        comeback to this specific property
-         */
-         '//*[@id="dateMenu"]');
-    // third object(date)
-    objectThree.retrieve_url();
-
-    console.log('hi')
-
-    objectFour =  new Object("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'id',
-        '//*[@id="bite-menu-dates"]')
-    // fourth object(daily bite)
-    objectFour.retrieve_url()
-
-    objectFive = new Object("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'id',
-        '//*[@id="allergen-key"]')
-    //fifth object(allergen key)
-    objectFive.retrieve_url();
-
-    objectSix = new Object("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'id',
-        '//*[@id="nutrition-calc-content"]')
-    //sixth object(nutrition calculator)
-    objectSix.retrieve_url();
-
-    objectSeven = new Object("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall",
-        'id',
-        '//*[@id="bite-menu"]')
-    //Seventh object(regarding entire daily menu and calculator)
-    objectSeven.retrieve_url();
-
-}
-retreive();
+objectOne.find_element();
