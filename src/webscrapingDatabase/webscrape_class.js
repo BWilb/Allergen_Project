5ec3972fe8b8c3aae6ca67a26b2436f8089588e4
BWilb <bryce.wilbur@my.simpson.edu>
2022-11-02 07:52:54 -0500

    webscrape_class made more malleable with sql statements

--- src/webscrapingDatabase/webscrape_class.js
+++ src/webscrapingDatabase/webscrape_class.js
@@ -1,37 +1,46 @@
 const mysql = require("mysql")
+const axios = require("axios")
+const cheerio = require("cheerio")
+
 class Object{
-    constructor(url, element, attribute, class_name){
-        this.url = url
+    constructor(element, attribute, class_name){
         this.element = element
         //variable for specific html element
         this.attribute = attribute
         //variable for elements attributes
         this.class_name = class_name
         //variable for elements class
-        this.axios = require("axios")
-        this.cheerio = require("cheerio")
     }
 }
 
-function find_element(object) {
+function finding_inputting(url, object, cals) {
 
-
-    object.axios(object.url)
+    axios(url)
         .then(response => {
             const html_script = response.data
-            //loading of HTML script into variable
             const values = []
-            const honey = object.cheerio.load(html_script)
+            const calVals = []
+            const honey = cheerio.load(html_script)
             honey(object.class_name, html_script).each(function(){
                 //function created to search through entire script
                 const text = honey(this).text()
                 const att = honey(this).find(object.element).attr(object.attribute)
+
 
                 values.push({
                     text
                 })
             })
 
+            honey(cals.class_name, html_script).each(function(){
+                const text = honey(this).text()
+
+                calVals.push({
+                    text
+                })
+            })
+
+            //const valuesTwo = values
             var con = mysql.createConnection({
                 //creation of connection to MySQL database
                     host: "allergen-db1.cutjc5tgzcff.us-east-2.rds.amazonaws.com",
@@ -47,16 +56,22 @@ function find_element(object) {
                     console.log("connected")
 
                     var databaseValues = [[]]
-                    //creation of variable that will store database values
+                    //2d variable
                     for(let i = 0; i < values.length; i++){
                         databaseValues[[i]] =
-                            [i, values[i].text, "yes", "yes", "no", "Pfeiffer"]
+                            [i, values[i].text, calVals[i].text, "yes", "yes", "no", "Pfeiffer"]
 
                     }
 
-                    con.query("INSERT INTO Food (Food_ID, Food_Name, Peanut_Free, Dairy_Free, Gluten_Free, Location) VALUES ?",
-                     [databaseValues], function(err, result){
+                    con.query("DELETE FROM Food", function(err, result){
+                        if(err){
+                            throw err
+                        }
+                        console.log("rows affected " + result.affectedRows)
+                    })
 
+                    con.query("INSERT INTO Food (Food_ID, Food_Name, Calories, Peanut_Free, Dairy_Free, Gluten_Free, Location) VALUES ?",
+                     [databaseValues], function(err, result){
                     //querying of webscraped data
                         if(err) {
                             throw err;
@@ -65,14 +80,20 @@ function find_element(object) {
                     })
 
                 })
+                for(let i = 0; i < values.length; i++){
+                    console.log(values[i])
+                }
 
 
         }).catch(err => console.log(err))
 }
 
 
-objectOne = new Object('https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall',
-    'a', 'href', '.get-nutritioncalculator')
+objectOne = new Object('a', 'href', '.get-nutritioncalculator')
 
     //foods
-find_element(objectOne)
+cals = new Object('a', 'href', '.get-nutrition')
+//calories of foods
+
+finding_inputting('https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14740&locationId=10395001&whereami=https://simpsondining-preview.sodexomyway.com/dining-near-me/pfieffer-dining-hall',
+objectOne, cals)
